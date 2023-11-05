3835dca485c193710211a794c1edf89d5f098378
jeanflo <jeanflorentcheveau@gmail.com>
2023-11-05 13:12:07 +0100

    Update wolf-linked-highlighter.user.js

--- dist/Perringaiden/wolf-linked-highlighter.user.js
+++ dist/Perringaiden/wolf-linked-highlighter.user.js
@@ -2,7 +2,7 @@
 // @author         Perringaiden
 // @name           Hide portals that aren't linked or fielded.
 // @category       Highlighter
-// @version        0.1
+// @version        0.2
 // @description    Hides any portal that doesn't have a link or field from it.
 // @id             wolf-linked-highlighter@Perringaiden
 // @updateURL      https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/Perringaiden/wolf-linked-highlighter.meta.js
