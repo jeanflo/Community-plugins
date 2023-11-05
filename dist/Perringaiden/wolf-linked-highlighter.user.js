2ac89777ecc5f88da33a4fe1d8e649123d70071d
jeanflo <jeanflorentcheveau@gmail.com>
2023-11-05 13:13:31 +0100

    Update wolf-linked-highlighter.user.js

--- dist/Perringaiden/wolf-linked-highlighter.user.js
+++ dist/Perringaiden/wolf-linked-highlighter.user.js
@@ -6,7 +6,7 @@
 // @description    Hides any portal that doesn't have a link or field from it.
 // @id             wolf-linked-highlighter@Perringaiden
 // @updateURL      https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/Perringaiden/wolf-linked-highlighter.meta.js
-// @downloadURL    https://raw.githubusercontent.com/IITC-CE/Community-plugins/master/dist/Perringaiden/wolf-linked-highlighter.user.js
+// @downloadURL    https://github.com/jeanflo/Community-plugins/raw/master/dist/Perringaiden/wolf-linked-highlighter.user.js
 // @match          https://intel.ingress.com/*
 // @grant          none
 // ==/UserScript==
